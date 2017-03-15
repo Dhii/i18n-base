656a9d13f1c81768e5065b18c655a7817cf8db78
Anton Ukhanev <xedin.unknown@gmail.com>
2017-03-15 19:02:26 +0100

    Added missed `use`

--- src/Exception/StringTranslationException.php
+++ src/Exception/StringTranslationException.php
@@ -4,6 +4,7 @@ namespace Dhii\I18n\Exception;
 
 use Dhii\I18n\TranslatorInterface;
 use Dhii\Data\ValueAwareInterface as Value;
+use Dhii\Util\String\StringableInterface as Stringable;
 
 /**
  * Represents an exception related to string translation.
